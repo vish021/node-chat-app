8d39a61e689f5f21b58b858c49d810a3309dd746
vishal <vishal.joshi.021@gmail.com>
2018-07-19 17:46:58 -0400

    add connection and disconnection event handlers

--- server/server.js
+++ server/server.js
@@ -1,13 +1,29 @@
 const path = require('path');
+const http = require('http');
 const publicPath = path.join(__dirname, '../public');
+const socketIO = require('socket.io');
 
 const express = require('express');
 const port = process.env.PORT || 3000;
 
-
 var app = express();
-app.use(express.static(publicPath));//for static middleware
+var server = http.createServer(app);
 
-app.listen(port, () => {
+//configure http server to use socket.io
+var io = socketIO(server);
+
+app.use(express.static(publicPath));//for static 
+
+//NOTE: when server goes down clients would keep polling server. But if browser is closed, socket server terminates connection
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('client disconnected');
+    });
+});
+
+//we are now using http server as opposed to express server
+server.listen(port, () => {
     console.log(`Server started at port ${port}`);
 });
