e0ff6ba4b630cbdd05a90805b98b539b0a1b7c9a
vishal <vishal.joshi.021@gmail.com>
2018-07-25 19:46:01 -0400

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,7 @@
+/** 
+ * Mustache is templating engine, you can pass object values dynamically to load in template
+ * 
+ * **/
 var socket = io();//initiating request to keep conn open
 
 socket.on('connect', function() {
@@ -10,22 +14,26 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
     var formattedTime =  moment(message.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
 
-    jQuery('#messages').append(li);
+    jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function(message) {
     var formattedTime =  moment(message.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My Current Location</a>');
-    
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href', message.url);
-    li.append(a);
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template, {
+        url: message.url,
+        from: message.from,
+        createdAt: message.createdAt
+    });
 
-    jQuery('#messages').append(li);
+    jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function (e) {
